ba9564f74ec65a3f5773041a10f09160a8f2b40b
gijuno <itx.ljh.developer@gmail.com>
2021-05-30 22:23:31 +0900

    feat: connect adapter and view model for place recyclerview

--- app/src/main/java/com/implude/niche/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/implude/niche/presentation/home/HomeFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.implude.niche.R
 import com.implude.niche.databinding.FragmentHomeBinding
+import com.implude.niche.domain.model.Place
+import com.implude.niche.presentation.base.PlaceAdapter
 import com.implude.niche.presentation.base.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
@@ -19,9 +22,25 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val baseAdapter = PlaceAdapter(this.requireContext())
+
+        val mLayoutManager = LinearLayoutManager(this.requireContext())
+        mLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        binding.recommendStoreRecyclerview.layoutManager = mLayoutManager
+
+        binding.recommendStoreRecyclerview.adapter = baseAdapter
+
+        baseAdapter.data = listOf(
+            Place("", "", "asdf", "", 0, 0, ""),
+            Place("", "", "asdf", "", 0, 0, ""),
+            Place("", "", "asdf", "", 0, 0, "")
+        )
+
+        baseAdapter.notifyDataSetChanged()
+
+
 
         return binding.root
     }
