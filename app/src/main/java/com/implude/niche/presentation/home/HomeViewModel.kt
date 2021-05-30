3b5d2ffd65baa2b1512b6eb44cee222d43743064
gijuno <itx.ljh.developer@gmail.com>
2021-05-30 22:22:39 +0900

    chore: remove useful codes in HomeViewModel.kt

--- app/src/main/java/com/implude/niche/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/implude/niche/presentation/home/HomeViewModel.kt
@@ -1,13 +1,13 @@
 package com.implude.niche.presentation.home
 
+import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
 }
+
