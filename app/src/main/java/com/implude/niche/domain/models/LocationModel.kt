43b65156c01b8a8d5b83ac1873ef2749a5715927
Beomjun Kim <nutyworks@gmail.com>
2021-06-03 07:40:00 +0900

    feat: add toLocationModel extension function

--- app/src/main/java/com/implude/niche/domain/models/LocationModel.kt
+++ app/src/main/java/com/implude/niche/domain/models/LocationModel.kt
@@ -1,6 +1,10 @@
 package com.implude.niche.domain.models
 
+import android.location.Location
+
 data class LocationModel(
     val latitude: Double,
     val longitude: Double,
 )
+
+fun Location.toLocationModel() = LocationModel(this.latitude, this.longitude)
