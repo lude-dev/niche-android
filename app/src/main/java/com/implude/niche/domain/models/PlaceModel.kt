f854d50cbad5b628f71bfe1007bda0d4427e8fc6
gijuno <itx.ljh.developer@gmail.com>
2021-06-05 00:21:10 +0900

    chore: set nullable PlaceModel(tags, comments)

--- app/src/main/java/com/implude/niche/domain/models/PlaceModel.kt
+++ app/src/main/java/com/implude/niche/domain/models/PlaceModel.kt
@@ -5,9 +5,9 @@ data class PlaceModel(
     val name: String,
     val location: LocationModel,
     val category: CategoryModel,
-    val tags: List<TagModel>,
+    val tags: List<TagModel>? = null,
     val owner: UserModel?,
     val verified: Boolean,
-    val comments: List<CommentModel>,
+    val comments: List<CommentModel>? = null,
     val hearted: Boolean,
 ) : DocumentModel
