362b81d0277e8a1ef87a6dcee1153710471dbe90
nutyworks <nutyworks@gmail.com>
2021-06-05 20:02:54 +0900

    fix(mapper): change mapper result type to a list of ReducedPlaceModel

--- app/src/main/java/com/implude/niche/data/mappers/NearPlacesMapper.kt
+++ app/src/main/java/com/implude/niche/data/mappers/NearPlacesMapper.kt
@@ -3,21 +3,21 @@ package com.implude.niche.data.mappers
 import com.implude.niche.NearPlacesQuery
 import com.implude.niche.domain.models.CategoryModel
 import com.implude.niche.domain.models.LocationModel
-import com.implude.niche.domain.models.PlaceModel
+import com.implude.niche.domain.models.ReducedPlaceModel
 import com.implude.niche.domain.models.TagModel
 
-class NearPlacesMapper : Mapper<NearPlacesQuery.Data, List<PlaceModel>> {
-    override fun map(data: NearPlacesQuery.Data): List<PlaceModel> = data.nearPlaces?.filterNotNull()?.map {
-        PlaceModel(
+class NearPlacesMapper : Mapper<NearPlacesQuery.Data, List<ReducedPlaceModel>> {
+    override fun map(data: NearPlacesQuery.Data): List<ReducedPlaceModel> = data.nearPlaces?.filterNotNull()?.map {
+        ReducedPlaceModel(
             id = it._id,
             name = it.name,
             location = LocationModel(it.location.lat ?: Double.NaN, it.location.lon ?: Double.NaN),
             category = CategoryModel(it.category._id, it.category.label ?: ""),
             tags = it.tags.filterNotNull().map { tag -> TagModel(tag._id, tag.label, null) },
-            owner = null,
             verified = it.verified,
-            comments = listOf(),
             hearted = it.hearted,
+            commentQuantity = it.commentQuantity,
+            heartQuantity = it.heartQuantity,
         )
     } ?: throw mappingFailedException
 }
