ab3a992b206e405e9e1316469458d38cc44bf80b
nutyworks <nutyworks@gmail.com>
2021-06-05 16:59:51 +0900

    feat: add heartQuantity field
    Also rearranged fields.

--- app/src/main/java/com/implude/niche/domain/models/PlaceModel.kt
+++ app/src/main/java/com/implude/niche/domain/models/PlaceModel.kt
@@ -2,12 +2,13 @@ package com.implude.niche.domain.models
 
 data class PlaceModel(
     override val id: String,
-    val name: String,
-    val location: LocationModel,
     val category: CategoryModel,
-    val tags: List<TagModel>,
-    val owner: UserModel?,
-    val verified: Boolean,
     val comments: List<CommentModel>,
+    val heartQuantity: Int,
     val hearted: Boolean,
+    val location: LocationModel,
+    val name: String,
+    val owner: UserModel?,
+    val tags: List<TagModel>,
+    val verified: Boolean,
 ) : DocumentModel
