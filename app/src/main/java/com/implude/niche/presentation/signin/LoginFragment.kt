c35b6fed5b9330b39af1a64c734cc204c93c6294
nutyworks <nutyworks@gmail.com>
2021-06-11 09:59:32 +0900

    feat(presentation): add email and login fragments

---
+++ app/src/main/java/com/implude/niche/presentation/signin/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.implude.niche.presentation.signin
+
+import com.implude.niche.R
+import com.implude.niche.databinding.FragmentLoginBinding
+import com.implude.niche.presentation.base.BaseFragment
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login)
