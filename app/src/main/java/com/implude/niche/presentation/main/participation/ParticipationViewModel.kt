bee9aafb8cb4958054d83c95007c8227c05dd7e9
nutyworks <nutyworks@gmail.com>
2021-06-11 22:30:49 +0900

    feat(vm): implement ParticipationViewModel

--- app/src/main/java/com/implude/niche/presentation/main/participation/ParticipationViewModel.kt
+++ app/src/main/java/com/implude/niche/presentation/main/participation/ParticipationViewModel.kt
@@ -3,11 +3,27 @@ package com.implude.niche.presentation.main.participation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.implude.niche.data.LocalDataStore
+import com.implude.niche.domain.models.NeedFixReportModel
+import com.implude.niche.domain.repositories.ParticipationRepository
+import com.implude.niche.presentation.util.SingleLiveEvent
 
-class ParticipationViewModel : ViewModel() {
+class ParticipationViewModel(
+    private val localDataStore: LocalDataStore,
+    private val participationRepository: ParticipationRepository,
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    val nearFixReportsFetchFailedEvent = SingleLiveEvent<Unit>()
+
+    private val _nearFixReports = MutableLiveData<List<NeedFixReportModel>>()
+    val nearFixReports: LiveData<List<NeedFixReportModel>> = _nearFixReports
+
+    fun fetchNearFixReports() {
+        val (lat, lon) = localDataStore.location ?: return
+        participationRepository.getNearFixReports(lat, lon).doOnSuccess {
+            _nearFixReports.value = it
+        }.doOnError {
+            nearFixReportsFetchFailedEvent.call()
+        }.subscribe()
     }
-    val text: LiveData<String> = _text
 }
