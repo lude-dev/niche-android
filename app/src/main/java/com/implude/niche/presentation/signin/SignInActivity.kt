bc89b24c224d577d2dabe99325d54067e2eae6d4
nutyworks <nutyworks@gmail.com>
2021-06-11 17:08:02 +0900

    chore(presentation): add navigation controller in sign in activity

--- app/src/main/java/com/implude/niche/presentation/signin/SignInActivity.kt
+++ app/src/main/java/com/implude/niche/presentation/signin/SignInActivity.kt
@@ -3,16 +3,21 @@ package com.implude.niche.presentation.signin
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.implude.niche.R
+import com.implude.niche.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
 
-    private val signInViewModel by viewModels<SignInViewModel>()
+    private val signInViewModel: SignInViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
 
-        supportFragmentManager.beginTransaction().add(R.id.fragment_container, EmailFragment()).commit()
+        val binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = fragment.navController
     }
 }
