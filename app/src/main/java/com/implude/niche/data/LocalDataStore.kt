205abd1f26e91910ab8d5c76cf88feb7aa7a1284
nutyworks <nutyworks@gmail.com>
2021-06-11 21:56:11 +0900

    chore(presentation): change HomeViewModel constructor

---
+++ app/src/main/java/com/implude/niche/data/LocalDataStore.kt
@@ -0,0 +1,7 @@
+package com.implude.niche.data
+
+import com.implude.niche.domain.models.LocationModel
+
+class LocalDataStore {
+    var location: LocationModel? = null
+}
--- app/src/main/java/com/implude/niche/presentation/main/home/HomeViewModel.kt
+++ app/src/main/java/com/implude/niche/presentation/main/home/HomeViewModel.kt
@@ -1,8 +1,16 @@
 package com.implude.niche.presentation.main.home
 
 import androidx.lifecycle.ViewModel
+import com.implude.niche.data.LocalDataStore
+import com.implude.niche.domain.repositories.LocalRepository
+import com.implude.niche.domain.repositories.PlaceRepository
+
+class HomeViewModel(
+    private val localDataStore: LocalDataStore,
+    private val localRepository: LocalRepository,
+    private val placeRepository: PlaceRepository,
+) : ViewModel() {
 
 class HomeViewModel : ViewModel() {
 
 }
-
