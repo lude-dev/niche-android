d9ed063aa098f3dea2d75422f2505fad7106271e
nutyworks <nutyworks@gmail.com>
2021-06-11 09:58:16 +0900

    feat(presentation): add SignInViewModel

---
+++ app/src/main/java/com/implude/niche/presentation/signin/SignInViewModel.kt
@@ -0,0 +1,14 @@
+package com.implude.niche.presentation.signin
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+
+    var email: String = ""
+    var password: String = ""
+    var passwordVerify: String = ""
+
+    fun onEmailEntered() {
+
+    }
+}
