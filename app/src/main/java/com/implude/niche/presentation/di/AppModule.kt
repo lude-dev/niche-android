67f577baa1833db11b263202643618d4dda1f150
nutyworks <nutyworks@gmail.com>
2021-06-13 15:39:07 +0900

    feat(di): define LocalRepository and LocationManager

--- app/src/main/java/com/implude/niche/presentation/di/AppModule.kt
+++ app/src/main/java/com/implude/niche/presentation/di/AppModule.kt
@@ -1,9 +1,14 @@
 package com.implude.niche.presentation.di
 
+import android.content.Context
+import android.location.LocationManager
 import com.implude.niche.data.LocalDataStore
+import com.implude.niche.data.repositories.LocalRepositoryImpl
+import com.implude.niche.domain.repositories.LocalRepository
 import com.implude.niche.presentation.main.home.HomeViewModel
 import com.implude.niche.presentation.main.participation.ParticipationViewModel
 import com.implude.niche.presentation.main.user.UserViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -13,4 +18,7 @@ val appModule = module {
     viewModel { UserViewModel() }
 
     single { LocalDataStore() }
+
+    single { androidContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager }
+    single<LocalRepository> { LocalRepositoryImpl(get()) }
 }
