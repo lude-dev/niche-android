fe045270c6f5925ff1149581e4c3ebf50b05c745
gijuno <itx.ljh.developer@gmail.com>
2021-06-13 15:27:17 +0900

    feat: create store detail activity

---
+++ app/src/main/java/com/implude/niche/StoreDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.implude.niche
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store_detail)
+    }
+}
