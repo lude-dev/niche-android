4a38466d047fb7d93fe75203ac59f05a5fb4b14b
nutyworks <nutyworks@gmail.com>
2021-06-18 01:46:03 +0900

    feat: add StoreDetailViewModel

--- app/src/main/java/com/implude/niche/presentation/di/AppModule.kt
+++ app/src/main/java/com/implude/niche/presentation/di/AppModule.kt
@@ -5,6 +5,7 @@ import android.location.LocationManager
 import com.implude.niche.data.LocalDataStore
 import com.implude.niche.data.repositories.LocalRepositoryImpl
 import com.implude.niche.domain.repositories.LocalRepository
+import com.implude.niche.presentation.StoreDetailViewModel
 import com.implude.niche.presentation.main.home.HomeViewModel
 import com.implude.niche.presentation.main.participation.ParticipationViewModel
 import com.implude.niche.presentation.main.user.UserViewModel
@@ -16,6 +17,7 @@ val appModule = module {
     viewModel { HomeViewModel(get(), get(), get()) }
     viewModel { ParticipationViewModel(get(), get()) }
     viewModel { UserViewModel() }
+    viewModel { StoreDetailViewModel(get()) }
 
     single { LocalDataStore() }
 
