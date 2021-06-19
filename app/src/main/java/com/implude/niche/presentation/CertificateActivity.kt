3aae185e7ff09c7da176217b99cb854daa90660f
gijuno <itx.ljh.developer@gmail.com>
2021-06-20 01:02:01 +0900

    feat: create certificate activity

---
+++ app/src/main/java/com/implude/niche/presentation/CertificateActivity.kt
@@ -0,0 +1,12 @@
+package com.implude.niche.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.implude.niche.R
+
+class CertificateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_certificate)
+    }
+}
