a9e28c28092a7f7a503add263b21d4a56f9f4a0a
gijuno <itx.ljh.developer@gmail.com>
2021-06-20 13:33:34 +0900

    feat: add go back arrow finish activity

--- app/src/main/java/com/implude/niche/presentation/EditStoreInfoActivity.kt
+++ app/src/main/java/com/implude/niche/presentation/EditStoreInfoActivity.kt
@@ -3,10 +3,21 @@ package com.implude.niche.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.implude.niche.R
+import com.implude.niche.databinding.ActivityEditStoreInfoBinding
 
 class EditStoreInfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditStoreInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_store_info)
+        binding = ActivityEditStoreInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goBackArrow.setOnClickListener {
+            finish()
+        }
+
     }
 }
